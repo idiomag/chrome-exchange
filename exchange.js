701d6e4f6ce5fd1bcaa12daa66233f2b784dd5dd
tremorwave <edward.barrow@idiomag.com>
2010-02-15 22:23:53 +0000

    Added supplementary comments

--- exchange.js
+++ exchange.js
@@ -2,18 +2,23 @@ var ownid;
 var remotelist = "http://assets.idiomag.com/ext/network.js";
 
 function showExchange() {
+	// locate id of extension (for exclusion)
 	ownid = chrome.extension.getURL().match(/\/\/(.*)\//)[1];
+	// load remote list of link data to include
 	loadScript(remotelist, loadAdvert);
 }
 
 function loadAdvert() {
+	// choose which link to use
 	var advert = choose(ownid);	
+	// display link
 	display(advert);
 }
 
 function choose() {
 	shuffle(exc);
 	for(var i = 0; i < exc.length; i++) {
+		// check not own link
 		if(exc[i].id != ownid) {
 			return exc[i];
 			break;
@@ -27,18 +32,23 @@ function shuffle(o){
 };
 
 function display(advert) {
+	// identify location for link
 	var div = document.getElementById("exchange");
+	// add link script
 	div.innerHTML = "Like this extension? Why not try <span id=\"exchange_link\" title=\""+advert.link+"\">"+advert.title+"</span>!";
 	var exlink = document.getElementById("exchange_link");
+	// add event listener for click
 	exlink.addEventListener("click", openExchange, true);
 }
 
 function openExchange(event) {
+	// open link in new tab
 	item = event.currentTarget;
     chrome.tabs.create({url: item.title});
 }
 
 function loadScript(url, callback){
+	// include remote list
     var script = document.createElement("script")
     script.type = "text/javascript";
 	script.onload = function(){

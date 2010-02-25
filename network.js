fd98ce5289d9033d08cdc501970bc219cdc6ac6c
tremorwave <edward.barrow@idiomag.com>
2010-02-25 10:18:55 +0000

    Added new partners

--- network.js
+++ network.js
@@ -5,6 +5,9 @@ var exc = [
 	{id:"cegndnhglpfbacbbjcmeejdoinjgiaae",title:"Huffington Post News Notifier",link:"http://bit.ly/d3a2wH"},
 	{id:"kmchifknbnnofpklgjohpiacgbghdjge",title:"Techcrunch News Notifier",link:"http://bit.ly/9SMWzl"},
 	{id:"ghlkcbmoicfncbihdpfaikgaonekelde",title:"E!Online News Notifier",link:"http://bit.ly/c8mVch"},
+	{id:"gfjagdckhokfnbikcdadlfcebeclecpj",title:"Ars Technica News Notifier",link:"http://bit.ly/bYzQHj"},
+	{id:"agbghcdpcpiiijcipfpgfdkfpohddceo",title:"Newsweek News Notifier",link:"http://bit.ly/dxFDBH"},
+	{id:"ciajakjjdopefddbfcjpiabklfjjdmjn",title:"Wired News Notifier",link:"http://bit.ly/aqRO9R"},
 	{id:"nbkjgckanjpbdmffcenbbikhngpnadep",title:"ChromePlay",link:"http://bit.ly/9DM75D"},
 	{id:"nfddebdieimglcclnchkjmfeekkeppja",title:"Yolink",link:"http://bit.ly/dsqkPk"}
 ];
